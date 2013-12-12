09c665aa80a3203d1e6de355febcd341537afac8
Stepler Paulo <paulobiscocho@gmail.com>
2013-12-12 16:28:44 +0800

    For Evaluation Result Functions (Evaluation Detail Model)

--- models/evaluation_detail.php
+++ models/evaluation_detail.php
@@ -1,8 +1,8 @@
 <?php
 class EvaluationDetail extends AppModel {
 	var $name = 'EvaluationDetail';
-	var $useDbConfig = 'evaluation';
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	var $virtualFields = array('frequency'=>'COUNT(*)',
+								'weight'=>'COUNT(*) * Option.value');  
 
 	var $belongsTo = array(
 		'Evaluation' => array(
@@ -27,4 +27,46 @@ class EvaluationDetail extends AppModel {
 			'order' => ''
 		)
 	);
+	
+	public function getWeightedMean(){
+		return $this->query("SELECT 
+								text,
+								ROUND(SUM(mul) / SUM(frequency),2) AS weighted_mean
+								FROM
+								  (SELECT 
+									`evaluation_details`.`question_id`,
+									`evaluations`.`form_id`,
+									`questions`.`text`,
+									COUNT(evaluations.`id`) AS frequency,
+									COUNT(`evaluation_details`.`option_id`) * `options`.`value` AS mul 
+								  FROM
+									`evaluation_details` 
+									INNER JOIN `options` 
+									  ON (
+										`evaluation_details`.`option_id` = `options`.`id`
+									  ) 
+									INNER JOIN `evaluations` 
+									  ON (
+										`evaluations`.`id` = `evaluation_details`.`evaluation_id`
+									  ) 
+									INNER JOIN `questions` 
+									  ON (
+										`questions`.`id` = `evaluation_details`.`question_id`
+									  ) 
+								  WHERE `options`.`value` > 0 
+								  GROUP BY 
+									`evaluation_details`.`question_id`,
+									`evaluation_details`.`option_id`) AS Question 
+								GROUP BY Question.question_id "
+			);
+	}
+
+	public function getFrequency(){
+		$conditions = array('Option.value >'=>0);
+		
+		$fields = array('EvaluationDetail.question_id','EvaluationDetail.frequency','Option.value','EvaluationDetail.weight','Question.text');
+		$group =array('EvaluationDetail.question_id','EvaluationDetail.option_id');
+	
+		return $this->find('all',compact('conditions','fields','group'));
+	}
 }
