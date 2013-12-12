a311aff1d9095ae45749a21f89648224ad04fecd
Stepler Paulo <paulobiscocho@gmail.com>
2013-12-12 16:29:55 +0800

    Key Header Model Adjusted

--- models/key_header.php
+++ models/key_header.php
@@ -1,8 +1,6 @@
 <?php
 class KeyHeader extends AppModel {
 	var $name = 'KeyHeader';
-	var $useDbConfig = 'key';
-	
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $hasMany = array(
 		'Key' => array(
