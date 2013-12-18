6af23d694391aa0fefcdb6f36a7ed5b852f46e2b
Stepler Paulo <paulobiscocho@gmail.com>
2013-12-18 13:49:57 +0800

    Added Print Key Pagination

--- vendors/print_keys.php
+++ vendors/print_keys.php
@@ -6,11 +6,13 @@ class KeysForm extends Formsheet{
 	protected static $_unit = 'in';
 	protected static $_orient = 'P';
 	protected static $_allot_subjects = 15;
+	
 	function KeysForm(){
 		$this->showLines = !true;
 		$this->FPDF(KeysForm::$_orient, KeysForm::$_unit,array(KeysForm::$_width,KeysForm::$_height));
 		$this->createSheet();
 	}
+	
 	protected function section($metrics){
 		$this->createGrid($metrics['base_x'] ,$metrics['base_y'],$metrics['width'],$metrics['height'],$metrics['rows'],$metrics['cols']);
 		$this->SetDrawColor(0);
@@ -18,6 +20,28 @@ class KeysForm extends Formsheet{
 			$this->Rect($metrics['base_x'],$metrics['base_y'],$metrics['width'],$metrics['height']);
 		}
 	}
+	
+	function hdr($data){
+		$metrics = array(
+			'base_x'=> 0,
+			'base_y'=> 0.25,
+			'height'=> 1,
+			'width'=> 8.5,
+			'cols'=> 45,
+			'rows'=> 6,
+		);
+		$this->section($metrics);	
+		$this->GRID['font_size']=9;
+		$date = new DateTime($data['KeyHeader']['created']);
+		$date = date_format($date, 'm/d/Y H:i:s');
+		
+		$this->leftText(1,1,'Form : '.$data['Form']['title'],'','');
+		
+		$this->leftText(1,2,'Key Count : '.count($data['Key']),'','');
+		$this->rightText(44,2,'Date Created : '.$date,'','');
+		$this->drawLine(4,'h',array(1,43));
+	}
+	
 	function keys($data){
 		$metrics = array(
 			'base_x'=> 0,
@@ -34,14 +58,23 @@ class KeysForm extends Formsheet{
 		$line3=7;
 		$x=1;
 		$gridBox=0;
-		$date = new DateTime($data['KeyHeader']['created']);
-		$date = date_format($date, 'm/d/Y H:i:s');
-		
-		$this->leftText(1,1,'Form : '.$data['Form']['title'],'','');
-		$this->leftText(1,2,'Date Created : '.$date,'','');
 		$this->drawLine(4,'h',array(1,43));
-	
+		$ctr=1;
+		$curr_page=1;
 		foreach($data['Key'] as $key){
+			if($line2>63){
+				$this->rightText(43,66,'Page '.$curr_page.' of '.count($data['Key'])/100,'');
+				$curr_page++;
+			
+				$this->createSheet();
+				$this->hdr($data);
+				$line1=5;
+				$line2=6;
+				$line3=7;
+				$x=1;
+				$gridBox=0;
+			}
+			
 			$this->leftText($x,$line1,'Key: '.$key['value'],'');
 			$this->leftText($x,$line2,'Status: '. $key['status_str'],'');
 			$this->drawLine($line3,'h',array($x,8.6));
@@ -56,6 +89,9 @@ class KeysForm extends Formsheet{
 				$gridBox=0;
 			}
 		}
+		
+		$total_page_count = count($data['Key'])/100;
+		$this->rightText(43,66,'Page '.$curr_page.' of '.ceil(count($data['Key'])/100),'');
 		
 	}
 }
--- views/key_headers/print_keys.ctp
+++ views/key_headers/print_keys.ctp
@@ -1,8 +1,8 @@
 <?php 
 App::import('Vendor','print_keys');
 
-
 $form=new KeysForm();
+$form->hdr($data);
 $form->keys($data);
 $form->output();
 ?>
