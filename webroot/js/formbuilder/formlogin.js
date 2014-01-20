28f7d60c79b6c103a060266c42416a1ed305605c
Stepler Paulo <paulobiscocho@gmail.com>
2014-01-20 14:31:17 +0800

    Adjusted Form Login Js
    Added Login Quiz Type Redirection

--- webroot/js/formbuilder/formlogin.js
+++ webroot/js/formbuilder/formlogin.js
@@ -1,3 +1,7 @@
+var EvalutionType = 3;
+var SurveyType = 2;
+var QuizType = 1;
+
 $(document).ready(function(){
 	//LOGIN EVENT
 	$(document).on('click','#LogInButton',function(){
@@ -8,20 +12,23 @@ $(document).ready(function(){
 					if(formReturn.data){
 						$('#FormId').val(formReturn.data.KeyHeader.form_id);
 						$('#ObjectId').val(formReturn.data.Key.id);
-
+						
 						if(formReturn.data.Key.status == '1'){
-							alert('Key already used');
+							alert('Warning: Used key');
 							return;
 						}
 						
-						if(formReturn.data.KeyHeader.Form.form_type_id==3){
+						if(formReturn.data.KeyHeader.Form.form_type_id==EvalutionType){
 							var action ='/formbuilder/evaluations/form';
 							$('#FormAction').attr('action',action);
 							$('#FormAction').submit();
-						}else if(formReturn.data.KeyHeader.Form.form_type_id==2){
+						}
+						else if(formReturn.data.KeyHeader.Form.form_type_id==QuizType){
+							var action ='/formbuilder/quizzes/form';
+							$('#FormAction').attr('action',action);
+							$('#FormAction').submit();
+						}else if(formReturn.data.KeyHeader.Form.form_type_id==SurveyType){
 							alert('This key is use for survey form type which is under constraction.Please contact developer for inquiries.');
-						}else if(formReturn.data.KeyHeader.Form.form_type_id==1){
-							alert('This key is use for quiz form type which is under constraction.Please contact developer for inquiries.');
 						}
 					}else{
 						alert('Invalid log in key');
