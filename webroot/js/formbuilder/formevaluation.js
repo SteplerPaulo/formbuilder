11ecb1b2e477b5f172f861e236b1efa857314ee2
Stepler Paulo <paulobiscocho@gmail.com>
2014-01-20 11:11:28 +0800

    Removed Login Event on formevalution Js

--- webroot/js/formbuilder/formevaluation.js
+++ webroot/js/formbuilder/formevaluation.js
@@ -1,38 +1,4 @@
 $(document).ready(function(){
-	//LOGIN EVENT
-	$(document).on('click','#LogInButton',function(){
-		if($('#EvaluationKey').val()){	
-			$('#EvaluationLoginForm').ajaxSubmit({
-				dataType:'json',
-				success:function(formReturn){
-					if(formReturn.data){
-						$('#FormId').val(formReturn.data.KeyHeader.form_id);
-						$('#ObjectId').val(formReturn.data.Key.id);
-
-						if(formReturn.data.Key.status == '1'){
-							alert('Key already used');
-							return;
-						}
-						
-						if(formReturn.data.KeyHeader.Form.form_type_id==3){
-							var action ='/formbuilder/evaluations/form';
-							$('#FormAction').attr('action',action);
-							$('#FormAction').submit();
-						}else if(formReturn.data.KeyHeader.Form.form_type_id==2){
-							alert('This key is use for survey form type which is under constraction.Please contact developer for more info.');
-						}else if(formReturn.data.KeyHeader.Form.form_type_id==1){
-							alert('This key is use for quiz form type which is under constraction.Please contact developer for more info.');
-						}
-					}else{
-						alert('Invalid log in key');
-					}
-				}
-			});
-		}else{
-			$('#EvaluationKey').focus();
-		}
-	});
-
 	//FORM SUBMIT EVENT
 	$(document).on('click','#EvaluationFormSubmitButton',function(){
 		if($('#EvaluationEvaluatee').val() !=''){
