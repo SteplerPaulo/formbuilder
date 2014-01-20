85536c8fc70b7f6bafb35cab0bd8d55d2c312df6
Stepler Paulo <paulobiscocho@gmail.com>
2014-01-20 11:13:45 +0800

    Quiz Detail Model & Controller Init Commit

---
+++ controllers/quiz_details_controller.php
@@ -0,0 +1,61 @@
+<?php
+class QuizDetailsController extends AppController {
+
+	var $name = 'QuizDetails';
+
+	function index() {
+		$this->QuizDetail->recursive = 0;
+		$this->set('quizDetails', $this->paginate());
+	}
+
+	function view($id = null) {
+		if (!$id) {
+			$this->flash(__('Invalid quiz detail', true), array('action' => 'index'));
+		}
+		$this->set('quizDetail', $this->QuizDetail->read(null, $id));
+	}
+
+	function add() {
+		if (!empty($this->data)) {
+			$this->QuizDetail->create();
+			if ($this->QuizDetail->save($this->data)) {
+				$this->flash(__('Quizdetail saved.', true), array('action' => 'index'));
+			} else {
+			}
+		}
+		$evaluations = $this->QuizDetail->Evaluation->find('list');
+		$questions = $this->QuizDetail->Question->find('list');
+		$options = $this->QuizDetail->Option->find('list');
+		$this->set(compact('evaluations', 'questions', 'options'));
+	}
+
+	function edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->flash(sprintf(__('Invalid quiz detail', true)), array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->QuizDetail->save($this->data)) {
+				$this->flash(__('The quiz detail has been saved.', true), array('action' => 'index'));
+			} else {
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->QuizDetail->read(null, $id);
+		}
+		$evaluations = $this->QuizDetail->Evaluation->find('list');
+		$questions = $this->QuizDetail->Question->find('list');
+		$options = $this->QuizDetail->Option->find('list');
+		$this->set(compact('evaluations', 'questions', 'options'));
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->flash(sprintf(__('Invalid quiz detail', true)), array('action' => 'index'));
+		}
+		if ($this->QuizDetail->delete($id)) {
+			$this->flash(__('Quiz detail deleted', true), array('action' => 'index'));
+		}
+		$this->flash(__('Quiz detail was not deleted', true), array('action' => 'index'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
---
+++ models/quiz_detail.php
@@ -0,0 +1,29 @@
+<?php
+class QuizDetail extends AppModel {
+	var $name = 'QuizDetail';
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	var $belongsTo = array(
+		'Evaluation' => array(
+			'className' => 'Evaluation',
+			'foreignKey' => 'evaluation_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Question' => array(
+			'className' => 'Question',
+			'foreignKey' => 'question_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Option' => array(
+			'className' => 'Option',
+			'foreignKey' => 'option_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
