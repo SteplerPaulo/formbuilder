e8d214a01e1377c304f380e087edb338fe0a8fca
Stepler Paulo <paulobiscocho@gmail.com>
2014-05-06 10:01:28 +0800

    User - Document Relationship Added

--- models/user.php
+++ models/user.php
@@ -1,4 +1,14 @@
 <?php
 class User extends AppModel {
 	var $name = 'User';
+	
+	var $hasOne = array(
+		'Document' => array(
+			'className' => 'Document',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
 }
